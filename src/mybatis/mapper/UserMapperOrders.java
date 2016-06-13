6e470db40e7cc80873931b2e3f47aeac3ac6238b
nishengwu <ni_shengwu@126.com>
2016-06-13 10:53:16 +0800

    多对多映射

--- src/mybatis/mapper/UserMapperOrders.java
+++ src/mybatis/mapper/UserMapperOrders.java
@@ -17,7 +17,7 @@ public interface UserMapperOrders {
 	//查询订单（关联用户）及订单明细
 	public List<Orders> findOrdersAndOrderDetailResultMap() throws Exception;
 	
-	//查询订单（关联用户）及订单明细
+	//查询用户购买商品信息
 	public List<User> findUserAndItemsResultMap() throws Exception;
 
 }
